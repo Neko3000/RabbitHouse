efcda5a439e633babffc2ec215dbb5771b832347
紅茶妖怪 <neko3000@live.com>
2015-07-11 20:01:01 +0900

    welcom page completed

---
+++ RabbitHouse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RabbitHouse.Startup))]
+namespace RabbitHouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
