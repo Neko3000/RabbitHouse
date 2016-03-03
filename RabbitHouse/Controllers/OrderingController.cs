c9c7a371e1260bfb4cb759f8ff59b0b5650d39fe
紅茶妖怪 <neko3000@live.com>
2016-03-03 16:20:59 +0900

    ordering page-user,filter+solid select

---
+++ RabbitHouse/Controllers/OrderingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RabbitHouse.Controllers
+{
+    public class OrderingController : Controller
+    {
+        // GET: Ordering
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
