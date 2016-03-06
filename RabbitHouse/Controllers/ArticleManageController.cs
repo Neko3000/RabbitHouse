e3e2074145a577f7054db5572911c0ed9636081d
紅茶妖怪 <neko3000@live.com>
2016-03-06 20:33:04 +0900

    prepared to create DBContext

---
+++ RabbitHouse/Controllers/ArticleManageController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RabbitHouse.Models;
+
+namespace RabbitHouse.Controllers
+{
+    public class ArticleManageController : Controller
+    {
+        private RabbitHouseContext db = new RabbitHouseContext();
+
+        // GET: ArticleManage
+        public ActionResult Index()
+        {
+            return View(db.Articles.ToList());
+        }
+
+        // GET: ArticleManage/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: ArticleManage/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArticleManage/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,ShortDescription,Description,Meta,UrlSlug,Published,PostTime,ModifyTime")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        // GET: ArticleManage/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: ArticleManage/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,ShortDescription,Description,Meta,UrlSlug,Published,PostTime,ModifyTime")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: ArticleManage/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: ArticleManage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RabbitHouse/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RabbitHouse.Models
+{
+    public class Article
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string ShortDescription { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Meta { get; set; }
+        public virtual string UrlSlug { get; set; }
+        public virtual bool Published { get; set; }
+        public virtual DateTime PostTime { get; set; }
+        public virtual DateTime? ModifyTime { get; set; }
+        public virtual ArticleCategory Category { get; set; }
+        public virtual IList<ArticleTag> Tags { get; set; }
+    }
+}
